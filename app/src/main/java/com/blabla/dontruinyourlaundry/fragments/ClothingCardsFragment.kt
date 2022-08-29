27e40a2a278c281ac9d68826075a8f8e6fd5bb16
pure <chechevichnaya@gmail.com>
2022-08-29 22:21:41 +0200

    cleaning imports

--- app/src/main/java/com/blabla/dontruinyourlaundry/fragments/ClothingCardsFragment.kt
+++ app/src/main/java/com/blabla/dontruinyourlaundry/fragments/ClothingCardsFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import com.blabla.dontruinyourlaundry.R
 import com.blabla.dontruinyourlaundry.adapters.RecyclerViewAdapterCards
